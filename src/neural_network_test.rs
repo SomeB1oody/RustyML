d02ab54467952bd0fa6319b415d527fe08585cde
SomeB1oody <stanyin64@gmail.com>
2025-04-09 11:23:26 -0700

    Add MAE test

--- src/neural_network_test.rs
+++ src/neural_network_test.rs
@@ -25,6 +25,29 @@ fn mse_test() {
 }
 
 #[test]
+fn mae_test() {
+    // 构造输入和目标张量，假设输入维度为 4，输出维度为 3，batch_size = 2
+    let x = Array::ones((2, 4)).into_dyn();
+    let y = Array::ones((2, 1)).into_dyn();
+
+    // 构建模型
+    let mut model = Sequential::new();
+    model.add(Dense::new(4, 3))
+        .add(Dense::new(3, 1));
+    model.compile(SGD::new(0.01), MeanAbsoluteError::new());
+
+    // 打印模型结构（summary）
+    model.summary();
+
+    // 训练模型
+    model.fit(&x, &y, 3);
+
+    // 使用 predict 进行前向传播预测
+    let prediction = model.predict(&x);
+    println!("预测结果: {:?}", prediction);
+}
+
+#[test]
 fn binary_cross_entropy_test() {
     // 构造输入和目标张量，假设输入维度为 4，输出维度为 3，batch_size = 2
     let x = Array::ones((2, 4)).into_dyn();
