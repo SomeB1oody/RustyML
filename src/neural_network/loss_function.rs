f06d72a5e9bdc6f0739fa236b6f7bd8858ee843c
SomeB1oody <stanyin64@gmail.com>
2025-04-09 11:22:01 -0700

    Add MAE support in loss function

--- src/neural_network/loss_function.rs
+++ src/neural_network/loss_function.rs
@@ -32,6 +32,27 @@ impl LossFunction for MeanSquaredError {
     }
 }
 
+pub struct MeanAbsoluteError;
+
+impl MeanAbsoluteError {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl LossFunction for MeanAbsoluteError {
+    fn compute_loss(&self, y_true: &Tensor, y_pred: &Tensor) -> f32 {
+        let diff = y_pred - y_true;
+        let abs_diff = diff.mapv(|x| x.abs());
+        abs_diff.sum() / (y_true.len() as f32)
+    }
+
+    fn compute_grad(&self, y_true: &Tensor, y_pred: &Tensor) -> Tensor {
+        let n = y_true.len() as f32;
+        (y_pred - y_true).mapv(|x| if x > 0.0 { 1.0/n } else if x < 0.0 { -1.0/n } else { 0.0 })
+    }
+}
+
 pub struct BinaryCrossEntropy;
 
 impl BinaryCrossEntropy {
