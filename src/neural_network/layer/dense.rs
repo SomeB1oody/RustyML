9c4a90b2333b90cc13fb560c3e05231e9edb3ba6
SomeB1oody <stanyin64@gmail.com>
2025-04-22 22:08:30 -0700

    Refactor activation function application in dense layer.
    Simplified the activation function logic by replacing the match-based parallel mapping with a single call to `Activation::apply_activation`. This improves code readability and maintainability while retaining the same functionality.

--- src/neural_network/layer/dense.rs
+++ src/neural_network/layer/dense.rs
@@ -155,23 +155,7 @@ impl Layer for Dense {
 
         if let Some(act) = &self.activation {
             // Apply activation function in parallel
-            let mut a = z.clone();
-
-            // Choose appropriate parallel mapping based on activation function type
-            match act {
-                Activation::ReLU => {
-                    a.par_mapv_inplace(|v| if v > 0.0 { v } else { 0.0 });
-                }
-                Activation::Sigmoid => {
-                    a.par_mapv_inplace(|v| 1.0 / (1.0 + (-v).exp()));
-                }
-                Activation::Tanh => {
-                    a.par_mapv_inplace(|v| v.tanh());
-                }
-                _ => {
-                    a = Activation::apply_activation(&z, act);
-                }
-            }
+            let a = Activation::apply_activation(&z, act);
 
             self.activation_output = Some(a.clone());
             a.into_dyn()
